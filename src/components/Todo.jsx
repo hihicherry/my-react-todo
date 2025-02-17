88c752859e0be795a7e7a3538849379b995385d6
Cherry Chung <bubibuuu@gmail.com>
2025-02-17 13:09:31 +0800

    Initial commit

---
+++ src/components/Todo.jsx
@@ -0,0 +1,31 @@
+import { MdEdit, MdDelete } from "react-icons/md";
+import EditForm from "./EditForm"
+
+function Todo({ todo, deleteTodo, toggleCompleted, toggleIsEditing, editTodo }) {
+  return todo.isEditing ? (
+    <EditForm todo={todo} editTodo={editTodo}/>
+  ) : (
+    <div className={`todo ${todo.isCompleted ? "completed" : ""}`}>
+      <p
+        onClick={() => {
+          toggleCompleted(todo.id);
+        }}
+      >
+        {todo.content}
+      </p>
+      <div>
+        <MdEdit
+        onClick={()=> {toggleIsEditing(todo.id)}} 
+        style={{ cursor: "pointer" }} />
+        <MdDelete
+          onClick={() => {
+            deleteTodo(todo.id);
+          }}
+          style={{ cursor: "pointer", marginLeft: "5px" }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
